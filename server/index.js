4ca8493b75b810499387dbf252d2586cddd03a49
ankitshukla-14 <ankitshukla9610@gmail.com>
2025-08-21 17:23:45 +0530

    Fix CORS policy to allow frontend requests

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:["http://localhost:3000", "https://studynotion-kohl.vercel.app"],
 		credentials:true,
 	})
 )
